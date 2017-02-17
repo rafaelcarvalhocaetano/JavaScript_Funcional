d773a7b8a50decd7bf3097c1c26d9dae80eccd7a
Rafael <rapha.pse@outlook.com>
2017-02-17 12:28:10 -0200

    Operadores ralacionais

---
+++ JS-003/001.js
@@ -0,0 +1,9 @@
+//var variavelComum = "Está é uma variavel em js.";
+//alert(variavelComum);
+
+
+	
+	var precoProduto = 4.35;
+	var quantidade = 5;
+	var total = precoProduto * quantidade;
+	alert(total);
