ce60324db0108a973708dfc837786a1f2015860b
Rafael <rapha.pse@outlook.com>
2017-02-17 12:42:27 -0200

    Uso do window Raiz

---
+++ JS-010/js.js
@@ -0,0 +1,8 @@
+var largura = window.winnerWidth;
+var titulo = window.document.title;
+
+
+window.alert("Teste 2");
+window.alert(largura);
+window.alert(titulo);
+//o windows é o método raiz do elemento
