7d2db792344c4d3a97cb8ce2c02f6408ad3e1557
Rafael <rapha.pse@outlook.com>
2017-02-17 12:26:06 -0200

    operadores

---
+++ JS-002/js.js
@@ -0,0 +1,32 @@
+
+
+document.write("Ola Mundo")
+
+var nome;
+var cliente;
+var quantidade = 40;
+var compra = null;
+nome = "Rafael";
+cliente = nome;
+
+document.write(cliente);
+
+function func01(){
+	var x = 10;
+	y = 20;
+	document.write(x);
+	document.write(y);
+};
+func01();
+function func02(){
+	y++;
+	document.write(y);
+	document.write(x);
+}
+document.write("<br>")
+var a= 10, b=4.50, c="Água", d=true, e;
+document.write("a é "+ typeof a +" <br/>");
+document.write("b é "+ typeof b+" <br/>");
+document.write("c é "+ typeof c+" <br/>");
+document.write("d é "+ typeof d+" <br/>");
+document.write("e é "+ typeof e+" <br/>");
