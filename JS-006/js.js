47c8280756dc87badd3fba9c5c287827a44e2fd7
Rafael Carvalho <rapha.pse@outlook.com>
2017-03-08 15:14:00 -0300

    Correção.

--- JS-006/js.js
+++ JS-006/js.js
@@ -1,15 +1,14 @@
 <h1>LOOP FOR - JS</h1>
-	<script type="text/javascript">
+	
 	for(var i=0; i<10; i++){
 		document.write(" <br>"+i);
 	}
-	</script>
+	
 
 	<h1>LOOP DO - WHILE</h1>
-	<script type="text/javascript">
+	
 	var i = 0;
 
 	while(i<10){
 		document.write(i +"<br>");
 	}
-	</script>
